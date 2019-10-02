e36a563fede2adb1d87d56f997e1a40f5483c2fd
timary13 <timary99@gmail.com>
2019-10-02 21:12:34 +0300

    feat: add task zeros solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function zeros(expression) {
-  // your solution
+
+    let flagTwo = false; //if have 2
+    let uneval = 0;
+    return expression.split('*')
+        .reduce(function (acum, fact) {
+            let num = parseInt(fact);
+            if (fact.endsWith("!!")) { //at eval !! we can get 5 from /10 and /50
+                if (!(num % 2)) {
+                    acum += Math.floor(num / 10);
+                    acum += Math.floor(num / 50);
+                    flagTwo = true;
+                }
+                else {
+                    while (num > 0) { //at uneval !! we get 5 from /5, then this count/2
+                        num = Math.floor(num / 5);
+                        uneval += Math.ceil(num / 2);
+                    }
+                }
+            }
+            else { //simple !
+                flagTwo = true;
+                while (num > 0) {
+                    num /= 5;
+                    acum += Math.floor(num);
+                }
+            }
+            if (flagTwo) { //if we haven't 2, maybe only 5
+                acum += uneval;
+                uneval = 0;
+            }
+            return acum;
+        }, 0);
+
 }
